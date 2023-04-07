ecda4cc7d18aecc24b391f6ede6d81ce1bd4c9bc
HeadandFolders <itsjannad@gmail.com>
2023-04-07 22:27:13 +0300

    working (templates) index page with 'tasks'

--- project_gains/WorkoutApp/views.py
+++ project_gains/WorkoutApp/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
 
 # Create your views here.
+
+tasks = ["foo", "bar", "baz"]
+def index(request):
+    context = {
+    'tasks': tasks
+    }
+    template = loader.get_template('WorkoutApp/index.html')
+    return HttpResponse(template.render(context, request))
