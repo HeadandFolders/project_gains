b7e56579c1a7a4ef10a519767f8faf0204555ae9
HeadandFolders <itsjannad@gmail.com>
2023-04-11 02:21:08 +0300

    created add workout form,  linked index/add pages

--- project_gains/WorkoutApp/urls.py
+++ project_gains/WorkoutApp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
-
+app_name = "WorkoutApp" #this is app name for the urls;it helps uniquely identify all of the urls
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("add", views.add, name="add")
 ]
--- project_gains/WorkoutApp/views.py
+++ project_gains/WorkoutApp/views.py
@@ -1,13 +1,41 @@
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
+from django.urls import reverse
 from django.shortcuts import render
 from django.template import loader
+from django import forms
+from django.forms import MultiWidget, TextInput 
 
 # Create your views here.
 
 tasks = ["foo", "bar", "baz"]
+
+class NewPostForm(forms.Form):
+    url = forms.URLField(label="workout_url")
+    hashtag = forms.CharField(widget=TextInput(attrs={'placeholder': '#UpperBody'}), label="workout_hashtag")
+    opinion = forms.CharField(label="workout_opinion")
+    rating = forms.IntegerField(max_value=10, min_value=1, label="workout_rating")
+    
+
 def index(request):
     context = {
     'tasks': tasks
     }
     template = loader.get_template('WorkoutApp/index.html')
+    return HttpResponse(template.render(context, request))
+
+def add(request):
+    template = loader.get_template('WorkoutApp/add.html') 
+    context = {
+        "form": NewPostForm()
+    }
+    if request.method == "POST":
+        form = NewPostForm(request.POST)
+        if form.is_valid():
+            workout = form.cleaned_data["hashtag"]
+            tasks.append(workout)
+            return HttpResponseRedirect(reverse("WorkoutApp:index"))
+        else:
+            return render(request, "WorkoutApp/add.html",{
+                "form": form
+            })
     return HttpResponse(template.render(context, request))
